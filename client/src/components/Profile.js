c5b4af56eb943b845ca746147793bd074fa34d55
ghadabouzaiene <ghadabouzaiene50@gmail.com>
2021-11-15 02:12:02 +0100

    modifying

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import { Avatar } from '@mui/material';
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store/store';
 import { Provider } from 'react-redux';
